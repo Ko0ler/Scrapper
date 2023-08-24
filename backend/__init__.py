a3835b072f3a3219ccf67f773e623013a373823b
Mohamed <ouedraogomohamed008@gmail.com>
2023-08-24 14:41:36 +0000

    Remove backend/

--- backend/__init__.py
+++
@@ -1,4 +0,0 @@
-# This is the backend package for the extraction tool.
-# It contains the main components of the Model-View-Controller (MVC) architecture,
-# including the model, view, controller, and exceptions.
-# The package provides the functionality to extract and process web content based on user input.
--- backend/test/__init__.py
+++
@@ -1,3 +0,0 @@
-# This is the test package for the extraction tool.
-# It contains the unit tests for the main components of the Model-View-Controller (MVC) architecture.
-# The package provides the functionality to test the extraction and processing of web content based on user input.
